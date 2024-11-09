288af7a477e8437897b3f361c0badb5ef0e423fa
Isaac Ferry <isaacferry5@gmail.com>
2024-11-09 09:32:51 -0800

    adding confirm email.

--- app/src/main/java/com/example/signinsignoutcs460/activities/SignUpActivity.java
+++ app/src/main/java/com/example/signinsignoutcs460/activities/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.signinsignoutcs460.activities;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,5 +36,33 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void SingUp(){
+        // check loading
+
+        // Post to Firebase
+
+    }
+
+    private Boolean isValidateSignUpDetails(){
+        if(binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Please Enter your Name");
+            return false;
+        } else if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Please Enter your Email");
+            return false;
+
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Please Enter valid Email");
+            return false;
+
+        }else{
+
+        }
     }
 }
